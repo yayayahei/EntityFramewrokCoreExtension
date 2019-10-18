d89a329327c471d3482d2773afa3c0d4ce35c07c
yayayahei <mmwcbz@msn.cn>
2019-10-18 17:39:40 +0800

    add build.ps1

---
+++ build.ps1
@@ -0,0 +1,8 @@
+﻿# usage
+# .\build.ps1 1.0.0 {apikey}
+$version=$args[0]
+$key=$args[1]
+$projName="EntityFramewrokCoreExtension"
+dotnet pack -p:PackageVersion=$version --include-symbols -o "packages" "./$($projName)/$($projName).csproj"
+dotnet nuget push "./packages/$($projName).$($version).symbols.nupkg" -k $key -s "https://www.nuget.org"
+
