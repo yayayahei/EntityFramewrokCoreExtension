cc90e081e7f893e0906ba79bc495bb2113e7d245
yayayahei <mmwcbz@msn.cn>
2019-10-18 17:21:03 +0800

    add EntityExtension.SupplyGuidKeyIfEmpty

---
+++ EntityFramewrokCoreExtension/EntityExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EntityFramewrokCoreExtension
+{
+    public static class EntityExtension
+        {
+            public static TEntity SupplyGuidKeyIfEmpty<TEntity>(this TEntity entity, IModel model)
+            {
+                var key = model.FindEntityType(typeof(TEntity)).FindPrimaryKey();
+                var properties = key.Properties;
+                if (properties.Count != 1) return entity;
+                var prop = properties[0];
+                if (prop.ClrType.UnwrapNullableType() != typeof(Guid)) return entity;
+                if (Guid.Empty.Equals(prop.PropertyInfo.GetValue(entity)))
+                {
+                    prop.PropertyInfo.SetValue(entity, Guid.NewGuid());
+                }
+    
+                return entity;
+            }
+    
+            public static Type UnwrapNullableType(this Type type) => Nullable.GetUnderlyingType(type) ?? type;
+        }
+}
